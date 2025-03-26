a81cdccfcaf94a8b01159ccf2efcd58eb9239254
Tato <codewtato@gmail.com>
2025-03-26 16:05:31 -0300

    Se agregaron comentarios a ej1

--- prueba.py
+++ prueba.py
@@ -1,7 +1,7 @@
-
+#Se definen las variables
 edad = int(input("Cual es tu edad?: "))
 nombre = input("Cual es tu nombre?: ")
-
+#Se checkean unas condiciones a cumplir
 if edad > 18:
   if nombre == "Tato":
     print("Hola Mundo!")
