668f0111f934bed949fd52a304d81fbbfd655d06
Tato <codewtato@gmail.com>
2025-03-26 15:58:58 -0300

    Comentamos el codigo y agregamos un else

---
+++ ej2.py
@@ -0,0 +1,12 @@
+
+#Pedimos nombre y edad por consola
+edad = int(input("Cual es tu edad?: "))
+nombre = input("Cual es tu nombre?: ")
+
+#Si edad es mayor a 18 y si nombre es tato, ponemos hola mundo, si el nombre no es tato ponemos Chau
+if edad > 18:
+  if nombre == "Tato":
+    print("Hola Mundo!")
+  else:
+      print("Chau")
+
